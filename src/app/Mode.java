9c313a8c12e8bfcbb2be3338f4e4851c60515529
Quu <91326783+Quu23@users.noreply.github.com>
2022-02-13 13:11:09 +0900

    make enumMode

---
+++ src/app/Mode.java
@@ -0,0 +1,9 @@
+package app;
+
+public enum Mode {
+    START,
+    SELECT,
+    GAME,
+    LOSE,
+    CLEAR
+}
--- src/app/ShortDungeon.java
+++ src/app/ShortDungeon.java
@@ -1,15 +1,23 @@
 package app;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import static app.Mode.*;
 
 public class ShortDungeon extends Application {
+
+    GraphicsContext gra;
+    Mode gameMode = START;
+
     @Override
     public void start(Stage stage) {
+
         stage.setTitle("ShortDungeon");
 
         Group root = new Group();
@@ -17,10 +25,21 @@ public class ShortDungeon extends Application {
 		Canvas cvs = new Canvas(400, 300);
 		root.getChildren().add(cvs);
         
+        gra=cvs.getGraphicsContext2D();
+
 		Scene scene = new Scene(root, 400, 300, Color.WHITE);
         
 		stage.setScene(scene);
 		stage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+           
+            @Override
+            public void handle(long arg0) {
+                
+                
+            }
+        };
  
     }
     public static void main(String[] args) {
