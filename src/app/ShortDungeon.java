82cf873073125e0f4da723c42ca6ee6c8e134bc0
Quu <91326783+Quu23@users.noreply.github.com>
2022-02-13 13:26:36 +0900

    make the LOSE mode

--- src/app/ShortDungeon.java
+++ src/app/ShortDungeon.java
@@ -7,13 +7,14 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import static app.Mode.*;
 
 public class ShortDungeon extends Application {
 
     GraphicsContext gra;
-    Mode gameMode = START;
+    Mode gameMode = LOSE;
 
     @Override
     public void start(Stage stage) {
@@ -36,10 +37,33 @@ public class ShortDungeon extends Application {
            
             @Override
             public void handle(long arg0) {
-                
-                
+                gra.setFill(Color.BLACK);
+                gra.fillRect(0, 0, 400, 300);
+                switch(gameMode){
+
+                    case START:
+                        break;
+                    case SELECT:
+                        break;
+                    case GAME:
+                        break;
+                    case LOSE:
+                        gra.setFont(new Font(50));
+                        gra.setFill(Color.RED);
+                        gra.fillText("GAMEOVER", 75,100);
+                        gra.setFont(new Font(25));
+                        gra.fillText("Push the Esc to close the Window", 23, 200);
+                        break;
+                    case CLEAR:
+                        break;
+                    default:
+                        gra.setFill(Color.BLACK);
+                        gra.fillRect(0, 0, 400, 300);
+                        break;
+                }              
             }
         };
+        timer.start();
  
     }
     public static void main(String[] args) {
