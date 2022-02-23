c55775f4ddec310a797f8fb2b3d7a6b2639b29f5
Quu <91326783+Quu23@users.noreply.github.com>
2022-02-23 20:42:12 +0900

    最強じゃなくなった

--- src/Item/DrogonSlayer.java
+++ src/Item/DrogonSlayer.java
@@ -3,7 +3,7 @@ package Item;
 import static chara.Attribute.*;
 
 public class DrogonSlayer extends Weapon {
-    //備考：最強の武器。ボス層のひとつ前の層にランダムで出現させる
+    //備考：ボス層のひとつ前の層にランダムで出現させる
     
     public DrogonSlayer(int x,int y,int identifier,boolean isOnMap){
         super(10, MASTER, x, y, identifier, isOnMap);
