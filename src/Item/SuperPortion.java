6cb1aad9602731e1375251ffae384748b25b315d
Quu <91326783+Quu23@users.noreply.github.com>
2022-02-24 20:32:27 +0900

    usePortionをオーバーライド

--- src/Item/SuperPortion.java
+++ src/Item/SuperPortion.java
@@ -2,20 +2,21 @@ package Item;
 
 import chara.Player;
 
-public class NormalPortion extends Portion{
+public class SuperPortion extends Portion {
     
-    NormalPortion(int x,int y,int identifier,boolean isOnMap){
-        super(10, x, y, identifier, isOnMap);
-    }    
+    SuperPortion(int x,int y,int identifier,boolean isOnMap){
+        super(25, x, y, identifier, isOnMap);
+    }
 
     @Override
     public int getId() {
-        return 1015;
+        return 1016;
     }
     @Override
     public String getName() {
-        return "普通のポーション";
+        return "すーぱーポーション";
     }
+
     @Override
     public void usePortion(Player player) {
         player.setHitPoint(player.getHitPoint()+this.getEffectPoint());
